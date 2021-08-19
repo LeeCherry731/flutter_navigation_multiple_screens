378d1d298bf12d777f5dd7d86bb759f6c619cce1
LeeCherry731 <sangevol0@gmail.com>
2021-08-19 14:29:30 +0700

    add gategories on the screaan

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_multiple_screens/dummy_data.dart';
+import 'package:flutter_navigation_multiple_screens/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(
+                title: e.title,
+                color: e.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
