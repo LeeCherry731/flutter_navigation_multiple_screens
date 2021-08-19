89e6bec69e3637e4c9c3f5ae1c1db1509655f70b
LeeCherry731 <sangevol0@gmail.com>
2021-08-19 14:44:52 +0700

    add colors theme and fonts theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,21 @@ class MyApp extends StatelessWidget {
       // hide the debug banner
       debugShowCheckedModeBanner: false,
       title: "Kindacode.com",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+            ),
+      ),
       home: CategoriesScreen(),
     );
   }
